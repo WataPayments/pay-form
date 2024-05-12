dc53be2e4eb43f1d711007b43fbe4f21937ac32b
RAZEX0LOL <khattayev00@mail.ru>
2024-05-12 21:45:21 +0400

    update

--- src/index.js
+++ src/index.js
@@ -5,10 +5,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import {createRoot} from "react-dom/client";
 const root = createRoot(document.getElementById('root'));
 root.render(
-    <Router basename="/pay-form">
+    <Router>
         <Routes>
-            <Route path="/:uuid" element={<App />} />
-            {/* Другие маршруты вашего приложения */}
+            <Route path="/pay-form/:uuid" element={<App />} />
         </Routes>
     </Router>
 );

32104373872155c1e23d387002d7322a55dfcb3c
RAZEX0LOL <khattayev00@mail.ru>
2024-05-12 18:20:36 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 // index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-ReactDOM.render(
+import {createRoot} from "react-dom/client";
+const root = createRoot(document.getElementById('root'));
+root.render(
     <Router>
         <Routes>
             <Route path="/pay-form/:uuid" element={<App />} />
         </Routes>
-    </Router>,
-    document.getElementById('root')
+    </Router>
 );

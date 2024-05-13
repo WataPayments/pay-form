2298bf81ef90ba8be29295e12fa04a3a01a98829
RAZEX0LOL <khattayev00@mail.ru>
2024-05-13 18:34:04 +0400

    update

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCqhvNuXaoNa8mHR8td_v5BqmRqk5T6o1g",
+    authDomain: "pay-form-c651d.firebaseapp.com",
+    projectId: "pay-form-c651d",
+    storageBucket: "pay-form-c651d.appspot.com",
+    messagingSenderId: "305165583833",
+    appId: "1:305165583833:web:a14e383393a22221ea45c8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, HashRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './App';
+import SuccessPage from './Components/SuccessPage';
+import ErrorPage from './Components/ErrorPage';
 
 ReactDOM.render(
-    <HashRouter>
+    <Router>
         <Routes>
             <Route path="/pay-form/:uuid" element={<App />} />
+            <Route path="/pay-form/success-pay/:uuid" element={<SuccessPage />} />
+            <Route path="/pay-form/error-pay/:uuid" element={<ErrorPage />} />
         </Routes>
-    </HashRouter>,
+    </Router>,
     document.getElementById('root')
 );

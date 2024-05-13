98cc4e35f0c89e91ea1798318f765d44c1e0c3ad
RAZEX0LOL <khattayev00@mail.ru>
2024-05-13 18:55:29 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, HashRouter, Route, Routes} from 'react-router-dom';
 import App from './App';
 import SuccessPage from './Components/SuccessPage';
 import ErrorPage from './Components/ErrorPage';
 
 ReactDOM.render(
-    <Router>
-        <Routes>
-            <Route path="/pay-form/:uuid" element={<App />} />
-            <Route path="/pay-form/success-pay/:uuid" element={<SuccessPage />} />
-            <Route path="/pay-form/error-pay/:uuid" element={<ErrorPage />} />
-        </Routes>
-    </Router>,
+    <HashRouter>
+        {/*<Routes>*/}
+        {/*    <Route path="/pay-form/:uuid" element={<App />} />*/}
+        {/*    <Route path="/pay-form/success-pay/:uuid" element={<SuccessPage />} />*/}
+        {/*    <Route path="/pay-form/error-pay/:uuid" element={<ErrorPage />} />*/}
+        {/*</Routes>*/}
+        <App />
+    </HashRouter>,
     document.getElementById('root')
 );
+

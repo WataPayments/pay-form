b8270333dbdddcbb8f90a9d99ee68281cd0d0026
RAZEX0LOL <khattayev00@mail.ru>
2024-05-14 04:11:59 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, BrowserRouter as Router, HashRouter, Route, Routes} from 'react-router-dom';
+import {
+    BrowserRouter,
+    BrowserRouter as Router,
+    createBrowserRouter,
+    HashRouter,
+    Route,
+    RouterProvider,
+    Routes
+} from 'react-router-dom';
 import App from './App';
 import SuccessPage from './Components/SuccessPage';
 import ErrorPage from './Components/ErrorPage';
 
-ReactDOM.render(
-    <BrowserRouter>
-        <Routes>
-            <Route exact path="/pay-form/:uuid" element={<App />} />
+const router=createBrowserRouter([
+    {
+        path:"pay-form/error-pay/:uuid",
+        element:<ErrorPage/>,
+    },
+    {
+        path:"/pay-form/:uuid",
+        element:<App/>,
+    },
+    {
+        path:"/pay-form/success-pay/:uuid",
+        element:<SuccessPage/>,
+    }
+])
 
-            <Route path="/pay-form/success-pay/:uuid" element={<SuccessPage />} />
-            <Route path="/pay-form/error-pay/:uuid" element={<ErrorPage />} />
-        </Routes>
-    </BrowserRouter>,
+ReactDOM.render(
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>,
     document.getElementById('root')
 );

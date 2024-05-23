05014254d50089323382575fc446c126883dec9b
RAZEX0LOL <khattayev00@mail.ru>
2024-05-23 22:05:45 +0400

    update

--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
-import Error from "../Images/Alert2 icon.svg";
+// import Error from "../Images/Alert2 icon.svg";
+import Alert from "../Images/Alert.svg";
 import '../Styles/ErrorPageStyle.css';
 import ApiClient from "../ApiClient";
 import logo from "../Images/Logo.svg";
@@ -40,20 +41,29 @@ export default function ErrorPage(props) {
         <div className={"container"}>
             <div className={"error-block"}>
                 <div className={"result-pay"}>
-                    <img src={Error} alt="Result-pay"/>
+                    {/*<img src={Error} alt="Result-pay"/>*/}
+                    <img src={Alert} alt="Result-pay"/>
                     <p>Ошибка оплаты!</p>
                     <div className={"error-reasons"}>
                         <p>Возможные причины:</p>
+                        {/*<ul>*/}
+                        {/*    <li>На карте недостаточно средств</li>*/}
+                        {/*    <li>Неправильно указаны реквизиты</li>*/}
+                        {/*    <li>Сессия истекла</li>*/}
+                        {/*</ul>*/}
                         <ul>
                             <li>На карте недостаточно средств</li>
                             <li>Неправильно указаны реквизиты</li>
-                            <li>Сессия истекла</li>
+                            <li>Ошибка банка эквайринга</li>
                         </ul>
+                        <div className="label-text">
+                            <p>Если у вас списались средства с карты, значит все прошло успешно, но банк не успел обработать транзакцию</p>
+                        </div>
                     </div>
                 </div>
                 <div>
                     <div className={"price-and-number-order"}>
-                        <p className={"price"}>{transactionData.amount}</p>
+                    <p className={"price"}>{transactionData.amount}</p>
                         <p className={"number"}>{/*{transactionData.order_number}*/}</p>
                     </div>
                     <div className={"link-and-info-order"}>

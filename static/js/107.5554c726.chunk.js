a3dc628444e1cbff8bc5266211b3f2e7c1a69009
NesQ1kx <111gosha11@gmail.com>
2024-05-29 23:49:51 +0400

    Updates

--- static/js/107.5554c726.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpay=self.webpackChunkpay||[]).push([[107],{2107:(e,s,a)=>{a.r(s),a.d(s,{default:()=>p});const p={}}}]);
-//# sourceMappingURL=107.5554c726.chunk.js.map
--- static/js/29.adb06ee9.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpay=self.webpackChunkpay||[]).push([[29],{5029:(e,s,a)=>{a.r(s),a.d(s,{default:()=>p});const p={}}}]);
-//# sourceMappingURL=29.adb06ee9.chunk.js.map
